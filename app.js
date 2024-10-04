56e37cf066244ee491806a1ecd944cc35e98d468
Agus-Iturria <agustin.iturria@gm2dev.com>
2024-10-04 00:11:18 -0300

    feat(app.js) add function to show pokemon image

--- app.js
+++ app.js
@@ -17,20 +17,45 @@ async function fetchPokemons(page) {
     }
 }
 
-function displayPokemons(pokemonList) {
-    pokemonCardsContainer.innerHTML = '';
-    pokemonList.forEach(pokemon => {
-        const card = `
-            <div class="pokemon-card">
-                <h3>${pokemon.name}</h3>
-            </div>
-        `;
-        pokemonCardsContainer.innerHTML += card;
-    });
+async function getPokemonDetails(pokemonUrl) {
+    try {
+        const response = await fetch(pokemonUrl);
+        if (response.ok) {
+            return await response.json();
+        } else {
+            console.log('Error en la respuesta:', response.status);
+            return null;
+        }
+    } catch (error) {
+        console.log('Error al obtener los detalles del Pokémon:', error);
+        return null;
+    }
 }
+
+async function displayPokemons(pokemonList) {
+    pokemonCardsContainer.innerHTML = '';
+    
+    for (const pokemon of pokemonList) {
+        const pokemonDetails = await getPokemonDetails(pokemon.url);
+
+        if (pokemonDetails) {
+            const sprite = pokemonDetails.sprites.front_default;
+            const types = pokemonDetails.types.map(typeInfo => typeInfo.type.name).join(', ');
+
+            const card = `
+                <div class="pokemon-card">
+                    <h3>${pokemon.name}</h3>
+                    <img src="${sprite}" alt="${pokemon.name}">
+                    <p>Tipo: ${types}</p>
+                </div>
+            `;
+            pokemonCardsContainer.innerHTML += card;
+        }
+    }
+}
+
 function searchPokemon() {
     const searchInput = document.getElementById('search-input').value.toLowerCase();
-    
     if (searchInput) {
         fetchPokemons(currentPage).then(() => {
             const pokemonCards = document.querySelectorAll('.pokemon-card');
